df560a14e874ad90931c6d6950c8316a268b1fb4
Alberto Rico <alrico88@gmail.com>
2020-03-19 21:27:03 +0100

    📗Add documentation

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# data-exporter
+<a name="Exporter"></a>
+
+## Exporter
+
+**Kind**: global class
+
+- [Exporter](#Exporter)
+  - [new Exporter()](#new_Exporter_new)
+  - _instance_
+    - [.save()](#Exporter+save)
+  - _static_
+    - [.Exporter](#Exporter.Exporter)
+      - [new Exporter(data, format, filename)](#new_Exporter.Exporter_new)
+
+<a name="new_Exporter_new"></a>
+
+### new Exporter()
+
+Exporter class
+
+<a name="Exporter+save"></a>
+
+### exporter.save()
+
+Triggers file download
+
+**Kind**: instance method of [<code>Exporter</code>](#Exporter)  
+<a name="Exporter.Exporter"></a>
+
+### Exporter.Exporter
+
+**Kind**: static class of [<code>Exporter</code>](#Exporter)  
+<a name="new_Exporter.Exporter_new"></a>
+
+#### new Exporter(data, format, filename)
+
+Creates an instance of Exporter.
+
+| Param    | Type                                                          |
+| -------- | ------------------------------------------------------------- |
+| data     | <code>Object</code>                                           |
+| format   | <code>&#x27;json&#x27;</code> \| <code>&#x27;csv&#x27;</code> |
+| filename | <code>String</code>                                           |
--- index.js
+++ index.js
@@ -8,63 +8,53 @@ const parse = require('json2csv').parse;
  */
 class Exporter {
 
-	/**
-	 *Creates an instance of Exporter.
-	 * @param {Object} data
-	 * @param {'json'|'csv'} format
-	 * @param {String} filename
-	 * @memberof Exporter
-	 */
-	constructor(data, format, filename) {
+  /**
+   *Creates an instance of Exporter.
+   * @param {Object} data
+   * @param {'json'|'csv'} format
+   * @param {String} filename
+   * @memberof Exporter
+   */
+  constructor(data, format, filename) {
 
-		/**
-		 * Converts JSON to CSV
-		 *
-		 * @param {Object[]} json
-		 * @returns {any} CSV
-		 */
-		this.convertToCsv = (json) => {
-			try {
-				let options = {
-					flatten: true,
-					delimiter: ';',
-				};
-				const csv = parse(json, options);
-				return csv;
-			} catch (err) {
-				throw err;
-			}
-		};
-		this.data = data;
-		this.format = format;
-		this.filename = filename;
-		this.mime = {
-			json: 'text/json;charset=utf-8',
-			csv: 'text/csv;charset=utf-8',
-		};
-	}
+    /**
+     * Converts JSON to CSV
+     *
+     * @private
+     * @param {Object[]} json
+     * @returns {any} CSV
+     */
+    this.convertToCsv = (json) =>
+      parse(json, {
+        flatten: true,
+        delimiter: ';',
+      });
+    this.data = data;
+    this.format = format;
+    this.filename = filename;
+    this.mime = {
+      json: 'text/json;charset=utf-8',
+      csv: 'text/csv;charset=utf-8',
+    };
+  }
 
-	/**
-	 * Triggers file download
-	 *
-	 * @memberof Exporter
-	 */
-	save() {
-		try {
-			let content =
-				this.format === 'json'
-					? JSON.stringify(this.data, null, 2)
-					: this.convertToCsv(this.data);
-			saveAs(
-				new Blob([content], {
-					type: this.mime[this.format],
-				}),
-				`${this.filename}.${this.format}`
-			);
-		} catch (err) {
-			throw err;
-		}
-	}
+  /**
+   * Triggers file download
+   *
+   * @memberof Exporter
+   */
+  save() {
+    let content =
+      this.format === 'json'
+        ? JSON.stringify(this.data, null, 2)
+        : this.convertToCsv(this.data);
+    saveAs(
+      new Blob([content], {
+        type: this.mime[this.format],
+      }),
+      `${this.filename}.${this.format}`
+    );
+  }
 }
 
 module.exports = Exporter;
